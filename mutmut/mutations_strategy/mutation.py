55135ecf0f0d2eb7a88434bfadc54f97d603fbc1
Viktor Hura <viktorhura.info@gmail.com>
2024-05-26 21:43:13 +0200

    Abstract method invocation exception

--- mutmut/mutations_strategy/mutation.py
+++ mutmut/mutations_strategy/mutation.py
@@ -3,11 +3,11 @@ from abc import ABC, abstractmethod
 class Mutation(ABC):
     @abstractmethod
     def mutate(self, *args, **kwargs):
-        pass
+        raise NotImplemented
 
     @abstractmethod
     def get_mutate_pointer(self):
         """
         Returns pointer to self.mutate in a dict, with key as either "children" or "value"
         """
-        pass
+        raise NotImplemented
